37e3597ba3c1950cc1114e842695fb4062e8f0b6
Artem <artiem.kulish.1997@mail.ru>
2023-06-17 12:11:21 +0300

    Git progect

--- Git_file.py
+++ Git_file.py
@@ -1,2 +1,15 @@
-slovo =int(input())
-print(slovo)
+slovo = int(input())
+simbol = input()
+slovo2 = int(input())
+if simbol == '+':
+    print(slovo + slovo2)
+elif simbol == '*':
+    print(slovo * slovo2)
+elif simbol == '/' and slovo != 0 and slovo2 != 0:
+    print(slovo / slovo2)
+elif simbol == '%' and slovo != 0 and slovo2 != 0:
+    print(slovo % slovo2)
+elif simbol == '-':
+    print(slovo - slovo2)
+elif simbol == '//' and slovo != 0 and slovo2 != 0:
+    print(slovo // slovo2)
